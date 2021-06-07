69c79973b920c9f7ae437d72850dfe25f90aecaf
xlord <h.rashidi128@gmail.com>
2021-06-08 02:58:38 +0430

    quality assurance

--- Quality-Assurance/advanced-node/public/client.js
+++ Quality-Assurance/advanced-node/public/client.js
@@ -1,9 +1,23 @@
+// This file's full path is /public/client.js
 $(document).ready(function () {
-  // Form submittion with new message in field with id 'm'
-  $('form').submit(function () {
-    var messageToSend = $('#m').val();
+	/* Global io */
+	let socket = io();
+	socket.on('user', (data) => {
+		$('#num-users').text(data.currentUsers + ' users online');
+		let message = data.name + (data.connected ? ' has joined the chat.' : ' has left the chat.');
+		$('#messages').append($('<li>').html('<b>' + message + '</b>'));
+	});
 
-    $('#m').val('');
-    return false; // prevent form submit from refreshing page
-  });
+	socket.on('chat message', (data) => {
+		console.log('socket.on 1');
+		$('#messages').append($('<li>').text(`${data.name}: ${data.message}`));
+	});
+
+	// Form submittion with new message in field with id 'm'
+	$('form').submit(function () {
+		let messageToSend = $('#m').val();
+		// Send message to server here?
+		$('#m').val('');
+		return false; // Prevent form submit from refreshing page
+	});
 });
